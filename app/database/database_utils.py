6d5fb49d29ab42b99fb114dc5eb091faff043b1d
raymonddevries <raymond.l.devries@gmail.com>
2020-08-10 15:45:33 -0700

    Added database_utils.py

---
+++ app/database/database_utils.py
@@ -0,0 +1,17 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from motor.core import AgnosticCollection
+
+DB_NAME = "skagit60"
+
+
+def get_client():
+    return AsyncIOMotorClient()
+
+
+def get_db(db_name: str):
+    return get_client()[db_name]
+
+
+def get_collection(collection_name: str):
+    return get_db(DB_NAME)[collection_name]
+
