d573715ce4d108088a73e0b8b94cc9a50a3e5764
raymonddevries <raymond.l.devries@gmail.com>
2020-08-11 08:47:52 -0700

    Refactored variable on pydantic Peak model.

--- app/models/peak.py
+++ app/models/peak.py
@@ -10,14 +10,14 @@ class Peak(BaseModel):
     display_name: constr(max_length=100)
     elevation: conint(ge=0, lt=30000)
     lat: confloat(ge=-90, le=90)
-    longitude: confloat(ge=-180, le=180)
+    long: confloat(ge=-180, le=180)
     peakbagger_link = HttpUrl
 
 
 class Tick(BaseModel):
-    climber: ObjectId
+    climber: str
     date: datetime.date
 
 
 class InterestedClimber(BaseModel):
-    climber: ObjectId
+    climber: str
