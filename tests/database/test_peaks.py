6b6d01d87f579bfedea0063de7c612137dd824ef
raymonddevries <raymond.l.devries@gmail.com>
2020-08-12 00:20:51 -0700

    Formatting change.

--- tests/database/test_peaks.py
+++ tests/database/test_peaks.py
@@ -8,7 +8,7 @@ from app.database.database_utils import get_collection
 
 @pytest.mark.asyncio
 async def test_create_peaks(fake_peak, fake_db):
-    result = await peaks.create_peak(fake_peak, fake_db)
+    result = await peaks.create_peak_db(fake_peak, fake_db)
     created_peak = await get_collection(peaks.COLLECTION, fake_db).find_one(
         ObjectId(result)
     )
@@ -20,5 +20,5 @@ async def test_create_peaks(fake_peak, fake_db):
 async def test_get_peaks(fake_peak, fake_db):
     inserts = [fake_peak.dict() for _ in range(5)]
     await get_collection(peaks.COLLECTION, fake_db).insert_many(inserts)
-    results = await peaks.get_peaks(fake_db)
+    results = await peaks.get_peaks_db(fake_db)
     assert results == inserts
